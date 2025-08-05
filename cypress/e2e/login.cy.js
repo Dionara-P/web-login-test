389cb1d787bf115bbf30f39c7ae33694c48fd06a
Dionara-P <dionara.romenia@gmail.com>
2025-08-05 20:48:21 -0300

    Adicionando mais um teste de login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -10,7 +10,15 @@ describe('Login', () => {
     cy.get('#login-form > :nth-child(3) > .col > .btn').click()
 
     //Assert 
-    cy.get('i.material-icons')
-      .should('exist');
+    cy.get('i.material-icons').should('exist');
+  })
+  it('Login com senha inválida não deve permitir acessar o sistema', () => {
+    //Act
+    cy.get('#login-form > :nth-child(1) > .input-field > label').click().type('grupo4')
+    cy.get('#login-form > :nth-child(2) > .input-field > label').click().type('senha@123')
+    cy.get('#login-form > :nth-child(3) > .col > .btn').click()
+
+    //Assert 
+    cy.get('i.material-icons').should('exist'); // Validar elemento com mensagem de erro
   })
 })
